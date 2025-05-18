3021a437347a4387a42e0eceb6a5d4b05a77f919
Aleksey Leonov <74103840+AlekseyLeonov031@users.noreply.github.com>
2025-05-18 19:51:14 +0500

    Add video player and episode card components
    Implemented a `VideoPlayer` module to support video playback with fullscreen controls. Introduced an `EpisodeCard` component, integrating the video player with episode details, and updated the `Seasons` component to display episodes using the new card format. Added related styles and icons to enhance UI functionality and visuals.

--- src/components/Seasons/Seasons.jsx
+++ src/components/Seasons/Seasons.jsx
@@ -2,14 +2,16 @@ import './Seasons.scss'
 import AccordionGroup from '@/components/AccordionGroup'
 import seasonItems from '@/components/Seasons/seasonItems'
 import Accordion from '@/components/Accordion'
+import EpisodeCard from '@/components/EpisodeCard'
 
 const Seasons = () => {
   return (
     <AccordionGroup
+      className="seasons"
       mode="dark"
       isOrderedList={false}
     >
-      {seasonItems.map(({ title, subtitle, episodes }, index) => (
+      {seasonItems.map(({title, subtitle, episodes}, index) => (
         <Accordion
           title={title}
           titleLevelClassName="h4"
@@ -20,7 +22,13 @@ const Seasons = () => {
           key={index}
           isArrowButton
         >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, ex?
+          <ul className="seasons__list">
+            {episodes.map((episode, index) => (
+              <li className="seasons__item" key={index}>
+                <EpisodeCard {...episode} />
+              </li>
+            ))}
+          </ul>
         </Accordion>
       ))}
     </AccordionGroup>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import OverlayMenu from "@/modules/OverlayMenu";
 import SliderCollection from "@/modules/SliderCollection";
 import TabsCollection from "@/modules/TabsCollection";
+import VideoPlayerCollection from "@/modules/VideoPlayerCollection";
 
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollection()
+new VideoPlayerCollection()
